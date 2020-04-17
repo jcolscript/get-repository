41ac03561c89fee99364326c4e711102debe3f9e
José Carlos <jcolscript@gmail.com>
2020-04-16 23:01:22 -0300

    trazendo repositorios e guardando no state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await api.get(`/repos/${this.state.newRepo}`);
+    const repository = {
+      name: data.name,
+      full_name: data.full_name,
+    };
+
+    this.setState({
+      repositories: [repository, ...this.state.repositories],
+    });
+  };
+  render() {
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="Adicionar repositório"
+          />
+          <SubmitButton>
+            <FaPlus color="#fff" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
