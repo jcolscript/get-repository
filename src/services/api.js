d9b4ca2481cdd16d489bb0c0f7b5a0bec8f03c24
José Carlos <jcolscript@gmail.com>
2020-04-18 23:17:47 -0300

    removendo dads

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.github.com/',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'token a6f83b2f546f33d25d06a6d61c933920494b5ca8',
-  },
 });
 
 export default api;
